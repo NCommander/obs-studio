7f0f9e008b551455a85cc6ce245b475993000132
Michael Casadevall <michael@mcprohosting.com>
2015-09-28 03:54:05 -0400

    Implement beginning of encoder setup
    Added codex to the property pages, and code pathes to get the initializatin
    interfaces from libvpx
    
    Signed-off-by: Michael Casadevall <michael@mcprohosting.com>

--- plugins/obs-vpx/obs-vpx.c
+++ plugins/obs-vpx/obs-vpx.c
@@ -22,10 +22,14 @@
 #include <util/platform.h>
 #include <obs-module.h>
 
+#include "vpx/vpx_encoder.h"
+#include "vpx/vp8cx.h"
+
 #define do_log(level, format, ...) \
 	blog(level, "[vpx encoder: '%s'] " format, \
 			obs_encoder_get_name(obsvpx->encoder), ##__VA_ARGS__)
 
+#define error(format, ...)  do_log(LOG_ERROR, format, ##__VA_ARGS__)
 #define warn(format, ...)  do_log(LOG_WARNING, format, ##__VA_ARGS__)
 #define info(format, ...)  do_log(LOG_INFO,    format, ##__VA_ARGS__)
 #define debug(format, ...) do_log(LOG_DEBUG,   format, ##__VA_ARGS__)
@@ -33,15 +37,19 @@
 /* ------------------------------------------------------------------------- */
 
 struct obs_vpx {
-	obs_encoder_t          *encoder;
+	obs_encoder_t		*encoder;
+	vpx_codec_ctx_t 	*vpx_ctx;
+	vpx_codec_iface_t	*vpx_iface;
+	vpx_codec_enc_cfg_t	*vpx_enc_cfg;
+	vpx_codec_flags_t 	*vpx_enc_flags;
 
-	DARRAY(uint8_t)        packet_data;
+	DARRAY(uint8_t)		packet_data;
 
-	uint8_t                *extra_data;
-	uint8_t                *sei;
+	uint8_t 			*extra_data;
+	uint8_t 			*sei;
 
-	size_t                 extra_data_size;
-	size_t                 sei_size;
+	size_t				extra_data_size;
+	size_t 				sei_size;
 
 	os_performance_token_t *performance_token;
 };
@@ -61,16 +69,63 @@ static void obs_vpx_destroy(void *data)
 
 static void obs_vpx_defaults(obs_data_t *settings)
 {
-
+	obs_data_set_default_string(settings, "codec", "vp8");
 }
+
+#define TEXT_CODEC    obs_module_text("Codec")
 
 static obs_properties_t *obs_vpx_props(void *unused)
 {
 	UNUSED_PARAMETER(unused);
 
 	obs_properties_t *props = obs_properties_create();
+	obs_property_t *list;
+
+	/**
+	 * For codex, we only return VP8 for now. We can encode VP9 with roughly
+	 * the same setup, but VP9 is a very bad choice for realtime encoding
+	 */
+
+	list = obs_properties_add_list(props, "codec", TEXT_CODEC,
+			OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_STRING);
+	obs_property_list_add_string(list, "vp8", "vp8");
 
 	return props;
+}
+
+static bool update_settings(struct obs_vpx *obsvpx, obs_data_t *settings)
+{
+	char *codec		= bstrdup(obs_data_get_string(settings, "codec"));
+	vpx_codec_iface_t* encoder;
+
+	/* Dump out configuration information to console */
+	blog(LOG_INFO, "---------------------------------");
+	if (codec && *codec)	info("codec: %s", codec);
+
+	/**
+	 * To setup the VP8 encoder, we need to do the following:
+	 *
+	 * 1. Point the correct codex initializer to vpx_codec_iface
+	 *
+	 * 2. Call vpx_codec_enc_config_default, and then populate the structure
+	 *    with all the settings we have interfaces for
+	 *
+	 * 3. Initialize the encoder with the codex and config sructs
+	 */
+
+	/* Step 1: Get the codex initializer */
+
+	if (strncmp (codec, "vp8", 3)) {
+		encoder = vpx_codec_vp8_cx();
+	} else {
+		error("Unknown codex configuration!");
+		goto fail;
+	}
+
+	return true;
+
+	fail:
+	return false;
 }
 
 static void obs_vpx_video_info(void *data, struct video_scale_info *info);
